db2e17ea636e3e597a0b2aed7829f130cc6f42b7
Narottam Goyal <narottamgoyal66@gmail.com>
2020-10-17 22:50:16 +0530

    added custom search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,28 @@ import { Data } from './data';
 })
 export class AppComponent {
   public numberFilterValue: any;
+  public firstNameFilterValue: any;
+  public lastNameFilterValue: any;
   public clientList = Data.peopeList;
+
+  /**
+   * numberFilter
+   */
+  public numberFilter(items: any[], filter: any): any {
+    return items.filter(item => (item.no + '').indexOf(filter) !== -1);
+  }
+
+  /**
+   * firstNameFilter
+   */
+  public firstNameFilter(items: any[], filter: any): any {
+    return items.filter(item => item.firstName.toLowerCase().indexOf(filter) !== -1);
+  }
+
+  /**
+   * lastNameFilter
+   */
+  public lastNameFilter(items: any[], filter: any): any {
+    return items.filter(item => item.lastName.toLowerCase().indexOf(filter) !== -1);
+  }
 }
--- src/pipe/customfilterPipe.ts
+++ src/pipe/customfilterPipe.ts
@@ -4,10 +4,10 @@ import { Pipe, PipeTransform } from '@angular/core';
     name: 'customfilterPipe'
 })
 export class MyFilterPipe implements PipeTransform {
-    transform(items: any[], filter: any): any {
+    transform(items: any[], filter: any, funcName: any): any {
         if (!items || !filter) {
             return items;
         }
-        return items.filter(item => (item.no + '').indexOf(filter) !== -1);
+        return funcName(items, filter);
     }
 }
